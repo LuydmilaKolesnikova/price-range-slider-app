210f89c9b084c52f65094af48fd2bd0f114c56cf
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-11-23 08:31:24 +0300

    add field data-max

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
+const max = document.querySelector(".range-slider").dataset.max;
+const delta = (138 * max) / 1000;
+
 let minValue = document.getElementById("min-value");
 let maxValue = document.getElementById("max-value");
 
 const rangeFill = document.querySelector(".range-fill");
 
 function validateRange() {
-  inputElements[0].value = Math.min(inputElements[0].value, inputElements[1].value-138);
-  inputElements[1].value = Math.max(Number(inputElements[0].value)+138, inputElements[1].value);
-  
+  inputElements[0].max = max;
+  inputElements[1].max = max;
+  inputElements[0].value = Math.min(
+    inputElements[0].value,
+    inputElements[1].value - delta
+  );
+  inputElements[1].value = Math.max(
+    Number(inputElements[0].value) + delta,
+    inputElements[1].value
+  );
 
   let minPrice = parseInt(inputElements[0].value);
   let maxPrice = parseInt(inputElements[1].value);
 
-  const minPercentage = ((minPrice) / 1000) * 100;
-  const maxPercentage = ((maxPrice) / 1000) * 100;
+  const minPercentage = (minPrice / max) * 100;
+  const maxPercentage = (maxPrice / max) * 100;
 
   rangeFill.style.left = minPercentage + "%";
   rangeFill.style.width = maxPercentage - minPercentage + "%";
 
   minValue.value = minPrice + " ₽";
   maxValue.value = maxPrice + " ₽";
+}
+
+function validateValueRange() {
+  let minPrice = parseInt(minValue.value);
+  let maxPrice = parseInt(maxValue.value);
+  if (minPrice < 0 || minPrice > maxPrice || !Number(minPrice)) {
+    minPrice = 0;
+  }
+  minValue.value = minPrice + " ₽";
+
+  if (maxPrice > max || maxPrice < maxPrice || !Number(maxPrice)) {
+    maxPrice = max;
+  }
+  maxValue.value = maxPrice + " ₽";
+
+  const minPercentage = (minPrice / max) * 100;
+  const maxPercentage = (maxPrice / max) * 100;
+  rangeFill.style.left = minPercentage + "%";
+  rangeFill.style.width = maxPercentage - minPercentage + "%";
+  inputElements[0].value = minPrice;
+  inputElements[1].value = maxPrice;
 }
 
 const inputElements = document.querySelectorAll(".range");
@@ -30,29 +61,5 @@ inputElements.forEach((element) => {
 minValue.addEventListener("blur", validateValueRange);
 maxValue.addEventListener("blur", validateValueRange);
 
-function validateValueRange() {
-  let minPrice = parseInt(minValue.value);
-  let maxPrice = parseInt(maxValue.value);
-  if (minPrice < 0 || minPrice > maxPrice || !Number(minPrice)) {
-    minPrice = 0;
-  } 
-   minValue.value = minPrice + " ₽";
-  
-  if (maxPrice > 1000 || maxPrice < maxPrice || !Number(maxPrice)) {
-    maxPrice = 1000;
-  } 
-  maxValue.value = maxPrice + " ₽";
-  
-  
-  const minPercentage = ((minPrice) / 1000) * 100;
-  const maxPercentage = ((maxPrice) / 1000) * 100;
-  rangeFill.style.left = minPercentage + "%";
-  rangeFill.style.width = maxPercentage - minPercentage + "%";
-  inputElements[0].value = minPrice;
-  inputElements[1].value = maxPrice;
-}
-
 validateRange();
 validateValueRange();
-
-
